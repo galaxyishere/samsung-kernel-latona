f328e4ba62681224e7d9e6ab3f85a37e4f38353c
netchip <netchip@netchip-dev.(none)>
2011-10-16 10:57:44 +0200

    Added my own build script for testing

---
+++ build_netchip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ x = "x$ANDROID_BUILD_TOP" ] ; then
+echo "Android build environment must be configured"
+exit 1
+fi
+. "$ANDROID_BUILD_TOP"/build/envsetup.sh
+
+# Make mrproper
+make mrproper
+
+# Set config
+make latona_galaxysl_defconfig
+
+# Make modules
+nice -n 10 make -j8 modules
+
+# Copy modules
+find -name '*.ko' -exec cp -av {} $ANDROID_BUILD_TOP/device/samsung/galaxysl/modules/ \;
+
+# Build kernel
+nice -n 10 make -j8 zImage
+
+# Copy kernel
+cp arch/arm/boot/zImage $ANDROID_BUILD_TOP/device/samsung/galaxysl/kernel
+
+# Make mrproper
+make mrproper
